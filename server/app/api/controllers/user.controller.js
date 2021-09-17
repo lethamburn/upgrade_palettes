275e9c21731a4cda83f1206d14d30e1d104f8423
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2021-09-17 12:17:23 +0200

    FEAT BASIC SERVER/USERS

---
+++ server/app/api/controllers/user.controller.js
@@ -0,0 +1,49 @@
+// Cargamos el modelo recien creado
+const User = require("../models/User");
+// Cargamos el módulo de bcrypt
+const bcrypt = require("bcrypt");
+// Cargamos el módulo de jsonwebtoken
+const jwt = require("jsonwebtoken");
+// Cargamos el fichero de los HTTPSTATUSCODE
+const HTTPSTATUSCODE = require("../../utils/httpStatusCode");
+
+// Codificamos las operaciones que se podran realizar con relacion a los usuarios
+module.exports = {
+  create: function (req, res, next) {
+    User.create(
+      {
+        name: req.body.name,
+        emoji: req.body.emoji,
+        email: req.body.email,
+        password: req.body.password,
+      },
+      function (err, result) {
+        if (err) next(err);
+        else
+          res.json({ status: 201, message: HTTPSTATUSCODE[201], data: null });
+      }
+    );
+  },
+  authenticate: function (req, res, next) {
+    User.findOne({ email: req.body.email }, function (err, userInfo) {
+      if (err) {
+        next(err);
+      } else {
+        if (bcrypt.compareSync(req.body.password, userInfo.password)) {
+          const token = jwt.sign(
+            { id: userInfo._id },
+            req.app.get("secretKey"),
+            { expiresIn: 3600 * 1000 }
+          );
+          res.json({
+            status: 200,
+            message: HTTPSTATUSCODE[200],
+            data: { user: userInfo, token: token },
+          });
+        } else {
+          res.json({ status: 400, message: HTTPSTATUSCODE[400], data: null });
+        }
+      }
+    });
+  },
+};
---
+++ server/server.js
@@ -0,0 +1,36 @@
+//Imports
+const express = require("express");
+const logger = require("morgan");
+const users = require("./app/api/routes/user.routes");
+const mongoose = require("./app/config/database");
+const HTTPSTATUSCODE = require("./app/utils/httpStatusCode");
+
+//Config
+const app = express();
+mongoose.connect();
+app.set("secretKey", "nodeRestApi"); // jwt secret token
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger("dev"));
+
+// public route
+app.use("/users", users);
+
+app.use(function (req, res, next) {
+  let err = new Error();
+  err.status = 404;
+  err.message = HTTPSTATUSCODE[404];
+  next(err);
+});
+
+// handle errors
+app.use(function (err, req, res, next) {
+  console.log(err);
+
+  if (err.status === 404)
+    res.status(404).json({ message: HTTPSTATUSCODE[404] });
+  else res.status(500).json({ message: HTTPSTATUSCODE[500] });
+});
+app.listen(3000, function () {
+  console.log("Node server listening on port 3000");
+});
