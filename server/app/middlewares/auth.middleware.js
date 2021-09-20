31056a92e9023219696c9679e8cc3c1dfbfaf29e
Alvaro <alvaroveneg@gmail.com>
2021-09-20 12:57:38 +0200

    req.user by token ok

--- server/app/middlewares/auth.middleware.js
+++ server/app/middlewares/auth.middleware.js
@@ -2,64 +2,44 @@ const jwt = require("jsonwebtoken")
 
 const isAuth = (req, res, next) => {
     const authorization = req.headers.authorization
-    console.log("req.headers.auth", req.headers.authorization) // bearer kdjfkdjfkjflkd...
-    console.log("id eq.headers.auth", req.headers.authorization.id) //esto es udefined
+    //console.log("req.headers.auth", req.headers.authorization) // bearer kdjfkdjfkjflkd...
+    //console.log("id eq.headers.auth", req.headers.authorization.id) //esto es udefined
     
     if(!authorization){
         res.statusCode = 401
         res.end("Falta la cabecera authorization")
         return 
     }
-   /*  const userLog = {
-        //id   : token.id,
-        "hola":"hola"
-
-    }
-    console.log('userlog', userLog)
-    req.userLog = userLog */
-
-    let trozos = authorization.split(" ")
-    if( trozos.length!=2 || trozos[0]!="Bearer"){
-        response.statusCode = 400
-        response.end("La cabecera authorization está mal construida")
+   
+    const splits = authorization.split(" ")
+    if( splits.length!=2 || splits[0]!="Bearer"){
+        res.statusCode = 400
+        res.end("La cabecera authorization está mal construida")
         return
     }
 
-
-    let jwtString = trozos[1]
-    console.log("JWT:"+jwtString)
-
+    const jwtString = splits[1]
+    //console.log("JWT:"+jwtString)
     try{
         var token = jwt.verify(jwtString, req.app.get("secretKey"));
         token.user.password=null;
-        console.log("token tras verify",token)  
+        //console.log("token tras verify",token)  
     } catch(e){
-        console.log(e)
+        //console.log(e)
         res.statusCode = 400
-        let error = {
+        const error = {
             codigo : 400,
             descripcion : "Error con el JWT: "+e.message
         }
         response.json(error)
         return
     }
-    let autoridad = {
+    const autoridad = {
         user   : token.user,
-       
     }
     //Se la asignamos al request
     req.autoridad = autoridad
     console.log("autoridad", autoridad)
-
-
-
-
-
-
-
-
-
-
     next()
 }
 
