82994df14c6b0b5ae1b44dbd9eaa30e59b323981
Alvaro <alvaroveneg@gmail.com>
2021-09-21 12:30:26 +0200

    mvp completed

--- server/app/api/controllers/user.controller.js
+++ server/app/api/controllers/user.controller.js
@@ -31,16 +31,16 @@ const createUser = async (req, res, next) => {
 }
 
 const authenticate = async (req, res, next) => {
-  try{
-    const userInfo = await User.findOne({email : req.body.email})
+  try {
+    const userInfo = await User.findOne({ email: req.body.email })
 
     if (bcrypt.compareSync(req.body.password, userInfo.password)) {
-      userInfo.password=null
+      userInfo.password = null
       const token = jwt.sign(
-        { 
+        {
           id: userInfo._id,
-          name: userInfo.name 
-        }, 
+          name: userInfo.name
+        },
         req.app.get("secretKey"),
         { expiresIn: "1h" }
       );
@@ -48,9 +48,9 @@ const authenticate = async (req, res, next) => {
       userInfo.password = null;
       //console.log('userInfo, ',userInfo)
       //console.log('token, ',token)
-      
-      
-      
+
+
+
       return res.json({
         status: 200,
         message: HTTPSTATUSCODE[200],
@@ -59,19 +59,19 @@ const authenticate = async (req, res, next) => {
     } else {
       return res.json({ status: 400, message: HTTPSTATUSCODE[400], data: null });
     }
-  }catch(err){
+  } catch (err) {
     return next(err);
   }
 }
-  
-const logout =  (req, res, next) => {
-  try{
-    return res.json({ 
-      status: 200, 
-      message: HTTPSTATUSCODE[200], 
-      token: null 
+
+const logout = (req, res, next) => {
+  try {
+    return res.json({
+      status: 200,
+      message: HTTPSTATUSCODE[200],
+      token: null
     });
-  }catch(err){
+  } catch (err) {
     return next(err)
   }
 }
